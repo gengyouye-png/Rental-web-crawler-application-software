09d404e50920db7d9b9b58376539081d82d9d13e
Reo <gengyouye@gmail.com>
2026-05-11 01:26:59 +0800

    抓取逢甲四周租屋處

--- clawler_測試連結.py
+++ clawler_測試連結.py
@@ -1,10 +1,9 @@
 import requests
 
-url = "https://www.591.com.tw/"
+url = "https://rent.591.com.tw/list?region=8&school=1481&kind=2&price=5000_10000&other=rental-subsidy"
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36" #反爬蟲
 }
 
 response = requests.get(url, headers=headers)
